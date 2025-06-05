bd85d2589f7b01c7b80515f6c738d83fcead7420
brou0049 <maxime.vaucois@gmail.com>
2025-06-05 16:49:29 +0200

    initial commit

---
+++ forestFire.py
@@ -0,0 +1,30 @@
+import random
+
+
+class Carte:
+    def __init__(self, taille=5):
+        self.taille = taille
+        self.elements_possibles = ['N', 'A', 'E']
+        self.grille = self.generer_carte()
+
+    def generer_carte(self, proba_arbre=0.6):
+        proba_eau = 0.2
+        proba_vide = 1 - proba_arbre - proba_eau
+        if proba_vide < 0:
+            raise ValueError("Les probabilités sont trop élevées. Elles doivent totaliser au maximum 1.")
+
+        # Construction de la liste pondérée
+        elements = (
+                ['F'] * int(proba_arbre * 100) +
+                ['E'] * int(proba_eau * 100) +
+                ['N'] * int(proba_vide * 100)
+        )
+
+        self.grille = [
+            [random.choice(elements) for _ in range(self.taille)]
+            for _ in range(self.taille)
+        ]
+
+    def afficher(self):
+        for ligne in self.grille:
+            print(' '.join(ligne))
---
+++ main.py
@@ -0,0 +1,7 @@
+# Exemple d'utilisation
+from forestFire import Carte
+
+if __name__ == "__main__":
+    carte = Carte()
+    carte.generer_carte(0.1)
+    carte.afficher()
